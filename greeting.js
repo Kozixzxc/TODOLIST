0268f60637be625a99dc204aa23f3d80ad28a81f
sangeobab <34633494+Kozixzxc@users.noreply.github.com>
2020-03-03 11:26:45 +0900

    함수 추가
    로컬스토리지 이름호출 구현

--- greeting.js
+++ greeting.js
@@ -5,16 +5,32 @@ const form = document.querySelector(".js-form"),
 const USER_LS="currentUser",
     SHOWING_CN = "showing"; //showing 클래스 갖고오기
 
+function saveName(text){
+    localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event){
+    event.preventDefault(); //submit default event delete
+    const currentValue = input.value;
+    paintGreeting(currentValue); // 받은 값 화면에 표출
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN); // 폼 숨기기(색칠때문에)
-    greeting.classList.add();
-    greeting.innerText = `Hello ${text}`
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
 }
 
 function loadName(){ //localstorage function
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
-        // 유저 없음
+      askForName();  // 유저 없음
     } else {
         paintGreeting(currentUser); //로컬스토리지 텍스트랑 같이 호출
     }
