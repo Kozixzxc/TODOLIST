21dd76c2bb96e8f094493fcb38f00f36cd584deb
sangeobab <34633494+Kozixzxc@users.noreply.github.com>
2020-03-04 10:55:08 +0900

    todo list 완성
    delete 버튼 활성화 , 로컬스토리지 리스트 사라지던 버그 고침..(const 썼음..)

--- todo.js
+++ todo.js
@@ -4,10 +4,21 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = []; //todos array에 todolist 저장
+let toDos = []; // todos array에 todolist 저장
 
-function saveToDos() { // todo를 가져와서 스토리지에 저장
-  localStorage.setItem(TODOS_LS , JSON.stringify(toDos)); //json.stringfy로 object->string변환
+function deleteToDo(event) { //target으로 잡은 button의 li의 id를 삭제함
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(function(toDo) { // foreach처럼 각각의 item에 실행시킴
+    return toDo.id !== parseInt(li.id); // 모든 todo의 li의 id와 같지 않을 때
+  });
+  toDos = cleanToDos;
+  saveToDos();
+}
+
+function saveToDos() { //todo를 가져와서 스토리지에 저장
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // json.stringfy로 object-> string 변환
 }
 
 function paintToDo(text) {
@@ -17,18 +28,20 @@ function paintToDo(text) {
   const newId = toDos.length + 1;
 
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
+
   span.innerText = text;
-  li.appendChild(span);
   li.appendChild(delBtn);
+  li.appendChild(span);
   li.id = newId;
 
   toDoList.appendChild(li);
   const toDoObj = {
     text: text,
-    id: toDos.length + 1
+    id: newId
   };
-  saveToDos()
-  toDos.push(toDoObj); //toDos array 안에 toDosObj 넣기
+  toDos.push(toDoObj); // toDos array안에 toDosObj 삽입
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -38,14 +51,13 @@ function handleSubmit(event) {
   toDoInput.value = "";
 }
 
-
 function loadToDos() {
   const loadedToDos = localStorage.getItem(TODOS_LS);
   if (loadedToDos !== null) {
     const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function(toDo) {
+    parsedToDos.forEach(function(toDo) { //forEach는 Array에 담겨있는 것들 각각에 한번씩 함수 실행
       paintToDo(toDo.text);
-    }); //forEach는Array에 담겨있는것들 각각에 한번씩 함수를 실행시킴
+    });
   }
 }
 
